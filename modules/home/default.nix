6c2d336c3d35d3e93bdaf4bf4a79565c05b5c869
luigi <virtual.employee@gmail.com>
2026-08-15 14:05:00 -0600

    Added niri to home-manager

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -18,9 +18,10 @@
     xdg.configFile."nvim".source = ../../config/nvim;
 
     # Link just the config.toml file, NOT the whole ~/.config/herdr dir:
-    # herdr writes runtime files there itself (logs, sockets, session.json,
     # .plugins.lock) which must stay writable and non-version-controlled.
     xdg.configFile."herdr/config.toml".source = ../../config/herdr/config.toml;
+    # Link the niri config gile
+    xdg.configFile."niri/config/kdl".source = ../../config/niri/config.kdl
 
     home.packages = with pkgs; [
         # Terminals
